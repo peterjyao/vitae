5c377d3bf43d8ed7ae50a3dd56c98ec8f80aa379
Peter Yao <peterjyao@gmail.com>
2021-07-29 09:39:33 +0200

    simplify site structure, add blog drafts

---
+++ docs/R/01_posts.R
@@ -0,0 +1,14 @@
+# https://distillery.rbind.io/posts/2021-03-16-postcards-distill/
+# 
+# library("distill")
+# create_post("first-post")
+# create_post("sterile-filtration")
+create_post("flow-sensors")
+create_post("yoctopuce")
+create_post("vim-tmux")
+create_post("photostability")
+create_post("base-R")
+create_post("misc-ggplot")
+create_post("qbd")
+create_post("python")
+create_post("vim-vifm")
